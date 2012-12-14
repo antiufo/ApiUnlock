b22dda1f7da908321d26802e0cf9ffb947687507
Andrea Martinelli <martinelli-andrea@outlook.com>
2012-12-14 19:28:20 +0100

    launcher page

--- ApiUnlock/App.xaml.cs
+++ ApiUnlock/App.xaml.cs
@@ -18,17 +18,6 @@ namespace ApiUnlock
             this.InitializeComponent();
         }
 
-
-        public delegate int MessageBoxWFunction(void* hwnd, [MarshalAs(UnmanagedType.LPWStr)] string lpText, [MarshalAs(UnmanagedType.LPWStr)] string lpCaption, uint uType);
-        public delegate void* GetForegroundWindowFunction();
-
-
-        private delegate int IntReturner();
-        private static byte[] returnNumber = new byte[]{
-            0xB8, 0x2A, 0x00, 0x00, 0x00, // MOV AEX, 42
-            0xC3 // RET
-        };
-
         protected unsafe override void OnLaunched(LaunchActivatedEventArgs args)
         {
 
@@ -45,24 +34,6 @@ namespace ApiUnlock
                     throw new Exception("Failed to create initial page");
             }
             Window.Current.Activate();
-
-
-            // Example: unrestricted reflection
-            ApiUnlocker.SetField(typeof(Exception), "_className", new Exception(), "example");
-
-            // Example: dynamic code generation
-            var memory = ApiUnlocker.AllocateExecutableMemory(1024);
-            Marshal.Copy(returnNumber, 0, new IntPtr(memory), returnNumber.Length);
-            var func = ApiUnlocker.GetDelegateForFunctionPointer<IntReturner>(memory);
-            var result = func();
-
-            // Example: Win32 APIs
-            Task.Delay(1000).GetAwaiter().OnCompleted(() =>
-            {
-                var MessageBoxW = ApiUnlocker.GetWin32Function<MessageBoxWFunction>("user32.dll", "MessageBoxW");
-                var GetForegroundWindow = ApiUnlocker.GetWin32Function<GetForegroundWindowFunction>("user32.dll", "GetForegroundWindow");
-                MessageBoxW(GetForegroundWindow(), "Hello from Win32! Our dynamically produced machine code returned: " + result, "Win32", 0);
-            });
         }
 
     }
--- ApiUnlock/MainPage.xaml.cs
+++ ApiUnlock/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,27 +14,57 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
 namespace ApiUnlock
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// A basic page that provides characteristics common to most applications.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public unsafe sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            lblResult.Text = string.Empty;
         }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.  The Parameter
-        /// property is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        public delegate int MessageBoxWFunction(void* hwnd, [MarshalAs(UnmanagedType.LPWStr)] string lpText, [MarshalAs(UnmanagedType.LPWStr)] string lpCaption, uint uType);
+        public delegate void* GetForegroundWindowFunction();
+
+        private delegate int IntReturner();
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            var memory = ApiUnlocker.AllocateExecutableMemory(1024);
+            var returnNumber = new byte[]{
+                0xB8, 0x2A, 0x00, 0x00, 0x00, // MOV AEX, 42
+                0xC3 // RET
+            };
+            Marshal.Copy(returnNumber, 0, new IntPtr(memory), returnNumber.Length);
+            var func = ApiUnlocker.GetDelegateForFunctionPointer<IntReturner>(memory);
+            var result = func();
+            lblResult.Text = @"Generated executable code:
+  MOV EAX, 42
+  RET
+
+Result of invocation: " + result;
         }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var MessageBoxW = ApiUnlocker.GetWin32Function<MessageBoxWFunction>("user32.dll", "MessageBoxW");
+            var GetForegroundWindow = ApiUnlocker.GetWin32Function<GetForegroundWindowFunction>("user32.dll", "GetForegroundWindow");
+            lblResult.Text = "Calling MessageBoxW...";
+            var result = MessageBoxW(GetForegroundWindow(), "Hello from Win32!", "Win32", 0);
+            lblResult.Text = "Return value of MessageBoxW: " + result;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            lblResult.Text = "Value of Environment.s_CheckedOSType: " + ApiUnlocker.GetField(typeof(Environment), "s_CheckedOSType", null);
+        }
+
     }
 }
